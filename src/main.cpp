39e6a2968120a1f66e4051ba92d62e9304aade67
Eragon <eragonaron@gmail.com>
2024-11-08 12:24:56 +0300

    one

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/timer.h>
+
+
+
+int main(void)
+{
+
+
+
+
+
+}
+
+// static void clock_setup(void)
+// {
+
+	
+	
+// }
+
+// //gdhghgdfh
+// static void timer1_setup(void)
+// {
+	
+
+
+
+// }
+
+// static void gpio_setup(void)
+// {
+
+
+
+
+
+// }
