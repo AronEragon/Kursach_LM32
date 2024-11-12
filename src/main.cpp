b2077f7397e601277b96690823402a35eca44d6f
filin_vg <eragonaron@yandex.ru>
2024-11-12 16:44:13 +0300

    Добавление новых файлов и комментариев в main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -39,20 +39,38 @@
 #define LD6 GPIOE, GPIO15
 
 
+/*
+lm35
+VCC --- +5V/+3V
+DAT --- PA0
+
+
+
+
+*/
+
 static void adc_setup(void)
 {
 	//ADC
 	rcc_periph_clock_enable(RCC_ADC12);
-	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOA); 
 	//ADC
-	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
+	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0); // DAT --- PA0
+	/*
+	GPIOA --- Беззнаковый int32. Идентификаторы портов GPIO
+	GPIO_MODE_ANALOG --- Беззнаковый int8. Режим работы(аналоговый)
+	GPIO_PUPD_NONE --- Беззнаковый int8. Конфигурация подтягивания/понижения вывода
+	GPIO0 --- Беззнаковый int16. Идентификаторы контактов.
+	 Если необходимо установить несколько контактов, используйте побитовое ИЛИ '|' чтобы разделить их. 
+	*/
 	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO1);
 	adc_power_off(ADC1);
 	adc_set_clk_prescale(ADC1, ADC_CCR_CKMODE_DIV2);
 	adc_set_single_conversion_mode(ADC1);
 	adc_disable_external_trigger_regular(ADC1);
 	adc_set_right_aligned(ADC1);
-	/* We want to read the temperature sensor, so we have to enable it. */
+	/* We want to read the temperature sensor, so we have to enable it.
+	Мы хотим прочитать показания датчика температуры, поэтому нам нужно его включить. */
 	adc_enable_temperature_sensor();
 	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_61DOT5CYC);
 	uint8_t channel_array[] = { 1 }; /* ADC1_IN1 (PA0) */
@@ -60,7 +78,8 @@ static void adc_setup(void)
 	adc_set_resolution(ADC1, ADC_CFGR1_RES_12_BIT);
 	adc_power_on(ADC1);
 
-	/* Wait for ADC starting up. */
+	/* Wait for ADC starting up. 
+	Подождите, пока запустится АЦП.*/
 	int i;
 	for (i = 0; i < 800000; i++)
 		__asm__("nop");
@@ -69,26 +88,30 @@ static void adc_setup(void)
 
 static void usart_setup(void)
 {
-	/* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART2. */
+	/* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART2. 
+	Включите тактовую частоту для порта GPIO A (для GPIO_USART2_TX) и USART2.*/
 	rcc_periph_clock_enable(RCC_USART2);
 	rcc_periph_clock_enable(RCC_GPIOA);
 
-	/* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. */
+	/* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit.
+	Настройте вывод GPIO GPIO_USART2_TX/GPIO9 на порту GPIO A для передачи. */
 	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
 	gpio_set_af(GPIOA, GPIO_AF7, GPIO2| GPIO3);
 
-	/* Setup UART parameters. */
-	usart_set_baudrate(USART2, 115200);
+	/* Setup UART parameters.
+	Настройте параметры UART. */
+	usart_set_baudrate(USART2, 115200); 
 	usart_set_databits(USART2, 8);
 	usart_set_stopbits(USART2, USART_STOPBITS_1);
 	usart_set_mode(USART2, USART_MODE_TX_RX);
 	usart_set_parity(USART2, USART_PARITY_NONE);
 	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
 
-	/* Finally enable the USART. */
+	/* Finally enable the USART. 
+	Наконец включите USART.*/
 	usart_enable(USART2);
 }
-
+//Работа светодиодов
 static void gpio_setup(void)
 {
 	rcc_periph_clock_enable(RCC_GPIOE);
